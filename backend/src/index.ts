7b08fa6fb762d5355cabc4c6ca32646dce556a3d
jaysenjonsin <jasoncjohnson5@yahoo.com>
2023-02-16 20:13:58 -0800

    update @types dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,19 @@
-import express from 'express'
+import express from 'express';
 import { globalErrorHandler } from './middleware/globalErrorHandler';
-import {config} from 'dotenv';
-config({path: '../.env'});
+import { config } from 'dotenv';
+config({ path: '../.env' });
 const PORT = process.env.PORT || 8080;
 
-
-
-const main = async() => {
+const main = async () => {
   const app = express();
 
-  app.use((_, res)=> res.status(404).send('page not found'))
+  app.use((_, res) => res.status(404).send('page not found'));
 
   app.use(globalErrorHandler);
 
-  app.listen(PORT, ()=> {
-    console.log(`Listening on port ${PORT} in ${process.env.NODE_ENV} mode`)
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT} in ${process.env.NODE_ENV} mode`);
   });
-}
+};
 
 main().catch((err) => console.log(err));
