542a29e788a42863cd781a3a0b925e76a7d455de
Jake Gray <gray@nat-wireless-guest-reg-153-53.bu.edu>
2023-02-23 11:38:39 -0500

    added jest to test server files

--- testServer/testServer.ts
+++ testServer/testServer.ts
@@ -1,15 +1,16 @@
-// import express from 'express'
-// import { createSchema, createYoga } from 'graphql-yoga'
-// const schema = require('./schema')
 
-// const app = express();
+import express from 'express'
+import { createSchema, createYoga } from 'graphql-yoga'
+const schema = require('./schema')
 
-// const yoga = createYoga({
-//     schema,
-// })
+const app = express();
 
-// app.use('/graphql', yoga)
-
-// app.listen(4000, () => {
-//     console.log('testServer running on port 4000')
-// })
+const yoga = createYoga({
+    schema,
+})
+//@ts-ignore
+app.use('/graphql', yoga)
+//@ts-ignore
+app.listen(4000, () => {
+    console.log('testServer running on port 4000')
+})
