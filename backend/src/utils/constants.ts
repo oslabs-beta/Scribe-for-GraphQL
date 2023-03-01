4fbfc6fcb5de663196023ec90bd010a0a2195330
jaysenjonsin <jasoncjohnson5@yahoo.com>
2023-02-28 21:48:18 -0800

    fix cors error

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const __prod__ = process.env.NODE_ENV === 'production';
-export const CLIENT_URL = 'http://localhost:5173/'
+export const CLIENT_URL = 'http://localhost:5173';
 export const COOKIE_NAME = 'asa';
