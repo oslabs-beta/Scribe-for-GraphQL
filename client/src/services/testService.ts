093552c3ee60e86bf2591643f6d73a2409686459
Pierce <109320021+PierceYu@users.noreply.github.com>
2023-03-02 22:50:27 -0500

    init prisma (#17)
    * front end testing page styling
    
    * Front end setup (#5)
    
    * tailwind and vite setup
    
    * convert tailwind to mui
    
    * testing page, homepage, register functionality setup
    
    * front end testing page styling
    
    * api call setup
    
    * Update README.md (#8)
    
    * fix cors error
    
    * contribution status for pierce
    
    * Front end setup (#14)
    
    * tailwind and vite setup
    
    * convert tailwind to mui
    
    * testing page, homepage, register functionality setup
    
    * TYPETESTS, TYPEDEFS FORMAT (#4)
    
    * working on type tests
    
    * formatted type tests, need to be checked and tested in jest
    
    * added jest to test server files
    
    * after thursday with mason :)
    
    * type checking, need to account for lists
    
    * typeDefs schema sent for Apollo style type checks :)
    
    * typetests done?
    
    * working on type tests
    
    * formatted type tests, need to be checked and tested in jest
    
    * added jest to test server files
    
    * after thursday with mason :)
    
    * type checking, need to account for lists
    
    * typeDefs schema sent for Apollo style type checks :)
    
    * typetests done?
    
    ---------
    
    Co-authored-by: Jake Gray <gray@Jakes-MacBook-Air.local>
    Co-authored-by: Jake Gray <gray@nat-wireless-guest-reg-153-53.bu.edu>
    
    * front end testing page styling
    
    * front end testing page styling (#6)
    
    * api call setup
    
    * Front end setup (#10)
    
    * front end testing page styling
    
    * api call setup
    
    ---------
    
    Co-authored-by: jaysenjonsin <jasoncjohnson5@yahoo.com>
    
    * fix cors error
    
    * contribution status for pierce
    
    ---------
    
    Co-authored-by: jaysenjonsin <jasoncjohnson5@yahoo.com>
    Co-authored-by: Mason Shelton <96751431+MasonS1012@users.noreply.github.com>
    Co-authored-by: soxg <94427592+soxg@users.noreply.github.com>
    Co-authored-by: Jake Gray <gray@Jakes-MacBook-Air.local>
    Co-authored-by: Jake Gray <gray@nat-wireless-guest-reg-153-53.bu.edu>
    Co-authored-by: jaysenjonsin <100107689+jaysenjonsin@users.noreply.github.com>
    
    * test generated
    
    ---------
    
    Co-authored-by: jaysenjonsin <jasoncjohnson5@yahoo.com>
    Co-authored-by: jaysenjonsin <100107689+jaysenjonsin@users.noreply.github.com>
    Co-authored-by: Mason Shelton <96751431+MasonS1012@users.noreply.github.com>
    Co-authored-by: soxg <94427592+soxg@users.noreply.github.com>
    Co-authored-by: Jake Gray <gray@Jakes-MacBook-Air.local>
    Co-authored-by: Jake Gray <gray@nat-wireless-guest-reg-153-53.bu.edu>

--- client/src/services/testService.ts
+++ client/src/services/testService.ts
@@ -2,8 +2,9 @@ import axios from "axios"
 import { API_URL } from "../utils/constants"
 
 export const generateTypeTest = async (userInput:string) => {
-  console.log('make post request to /typetest')
-  // console.log(API_URL + 'typeTest')
-  const { data } = await axios.post(API_URL + 'typeTest', userInput,{withCredentials:true});
+  console.log('make post request to /typeTest')
+  console.log('API', API_URL)
+  console.log(API_URL + 'typeTest')
+  const { data } = await axios.post(API_URL + 'typeTest', {schema:userInput},{withCredentials:true});
   return data;
 }
