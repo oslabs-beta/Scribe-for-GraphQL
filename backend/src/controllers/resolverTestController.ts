792ab3b78c357bcc878fa99621790d8cb03d1ec7
Jake Gray <gray@Jakes-MacBook-Air.local>
2023-03-06 22:57:47 -0500

    resolver tests added

--- backend/src/controllers/resolverTestController.ts
+++ backend/src/controllers/resolverTestController.ts
@@ -18,13 +18,32 @@ export const generateResolverTests = async (
     console.log('resolvers', resolvers)
 
 
-    const ResolverFrontBoiler = `describe('Resolvers return the correct information', () => {
-        const schema = makeExecutableSchema({ schema, resolvers })
-        const mocks = {
-            Int: () => 6,
-            Float: () => 22.7,
-            String: () => 'Hello',
-          };`    
+    const ResolverFrontBoiler = `describe('Resolvers return the correct values', () => {
+        const schema = makeExecutableSchema({ typeDefs, resolvers})
+        const mocks = { 
+          String: () => casual.sentence,
+          Int: () => casual.integer(1,100),
+          Float: () => 22.7,
+          Boolean: () => casual.boolean,
+          /*
+          If you'd like more specific mocks for any of your fields, add them below, using this as an example:
+          
+          User: () => ({ 
+            id: casual.uuid, 
+            name: casual.name, 
+            email: casual.email, 
+            age: casual.integer(18,100),
+          }),
+      
+          Please refer to the npm package 'casual' for random javascript generator calls.
+          */
+        }
+        const preserveResolvers = true;
+        const mockedSchema = addMocksToSchema({
+          schema,
+          mocks,
+          preserveResolvers
+        })`    
     const ResolverEndBoiler = `
     })`    
     res.status(200).json(resolvers)
