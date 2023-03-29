ce36eb8a5dcee583fef2b061e655fddeb6ec5eb7
Pierce Yu <pierceyu0315@live.com>
2023-03-29 15:12:57 -0400

    added animation to signin page

--- client/src/pages/Signin.tsx
+++ client/src/pages/Signin.tsx
@@ -4,55 +4,60 @@ import * as Components from '../styles/SliderStyle';
 function signinAndRegister() {
   const [signIn, toggle] = useState<boolean>(true);
   return (
-    <div id='SliderContainer'>
-      <Components.Container>
-        <Components.SignUpContainer signinIn={signIn}>
-          <Components.Form>
-            <Components.Title>Create Account</Components.Title>
-            <Components.Input type='text' placeholder='Full Name' />
-            <Components.Input type='email' placeholder='Email' />
-            <Components.Input type='password' placeholder='Password' />
-            <Components.Input type='password' placeholder='Confirm Password' />
-            <Components.Button>Sign Up</Components.Button>
-          </Components.Form>
-        </Components.SignUpContainer>
+    <div id='signinBackground'>
+      <div id='sliderContainer'>
+        <Components.Container>
+          <Components.SignUpContainer signinIn={signIn}>
+            <Components.Form>
+              <Components.Title>Create Account</Components.Title>
+              <Components.Input type='text' placeholder='Full Name' />
+              <Components.Input type='email' placeholder='Email' />
+              <Components.Input type='password' placeholder='Password' />
+              <Components.Input
+                type='password'
+                placeholder='Confirm Password'
+              />
+              <Components.Button>Sign Up</Components.Button>
+            </Components.Form>
+          </Components.SignUpContainer>
 
-        <Components.SignInContainer signinIn={signIn}>
-          <Components.Form>
-            <Components.Title>Sign in</Components.Title>
-            <Components.Input type='email' placeholder='Email' />
-            <Components.Input type='password' placeholder='Password' />
-            <Components.Anchor href='#'>
-              Forgot your password?
-            </Components.Anchor>
-            <Components.Button>Sign In</Components.Button>
-          </Components.Form>
-        </Components.SignInContainer>
+          <Components.SignInContainer signinIn={signIn}>
+            <Components.Form>
+              <Components.Title>Sign in</Components.Title>
+              <Components.Input type='email' placeholder='Email' />
+              <Components.Input type='password' placeholder='Password' />
+              <Components.Anchor href='#'>
+                Forgot your password?
+              </Components.Anchor>
+              <Components.Button>Sign In</Components.Button>
+            </Components.Form>
+          </Components.SignInContainer>
 
-        <Components.OverlayContainer signinIn={signIn}>
-          <Components.Overlay signinIn={signIn}>
-            <Components.LeftOverlayPanel signinIn={signIn}>
-              <Components.Title>Welcome Back!</Components.Title>
-              <Components.Paragraph>
-                Have an account already? Sign in to continue!
-              </Components.Paragraph>
-              <Components.GhostButton onClick={() => toggle(true)}>
-                Sign In
-              </Components.GhostButton>
-            </Components.LeftOverlayPanel>
+          <Components.OverlayContainer signinIn={signIn}>
+            <Components.Overlay signinIn={signIn}>
+              <Components.LeftOverlayPanel signinIn={signIn}>
+                <Components.Title>Welcome Back!</Components.Title>
+                <Components.Paragraph>
+                  Have an account already? Sign in to continue!
+                </Components.Paragraph>
+                <Components.GhostButton onClick={() => toggle(true)}>
+                  Sign In
+                </Components.GhostButton>
+              </Components.LeftOverlayPanel>
 
-            <Components.RightOverlayPanel signinIn={signIn}>
-              <Components.Title>Hello, Friend!</Components.Title>
-              <Components.Paragraph>
-                Create an account and start generating tests!
-              </Components.Paragraph>
-              <Components.GhostButton onClick={() => toggle(false)}>
-                Sign Up
-              </Components.GhostButton>
-            </Components.RightOverlayPanel>
-          </Components.Overlay>
-        </Components.OverlayContainer>
-      </Components.Container>
+              <Components.RightOverlayPanel signinIn={signIn}>
+                <Components.Title>Hello, Friend!</Components.Title>
+                <Components.Paragraph>
+                  Create an account and start generating tests!
+                </Components.Paragraph>
+                <Components.GhostButton onClick={() => toggle(false)}>
+                  Sign Up
+                </Components.GhostButton>
+              </Components.RightOverlayPanel>
+            </Components.Overlay>
+          </Components.OverlayContainer>
+        </Components.Container>
+      </div>
     </div>
   );
 }
