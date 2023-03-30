edb4b86e4e81d1823977eebb1320e8ff6e35bcd3
jaysenjonsin <jasoncjohnson5@yahoo.com>
2023-03-29 17:58:26 -0700

    begin auth on new page

--- client/src/pages/Signin.tsx
+++ client/src/pages/Signin.tsx
@@ -1,8 +1,115 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as Components from '../styles/SliderStyle';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { RootState, AppDispatch } from '../app/store';
+import { login, reset } from '../features/authSlice';
+import { zodResolver } from '@hookform/resolvers/zod';
+import Swal from 'sweetalert2';
+import { registerUser } from '../services/authService';
 
-function signinAndRegister() {
+const loginFormSchema = z.object({
+  email: z.string().min(1, 'email required'),
+  password: z.string().min(1, 'Please enter your password'),
+});
+export type loginFormSchemaType = z.infer<typeof loginFormSchema>;
+
+const registerFormSchema = z
+  .object({
+    name: z.string().min(1, 'name required'),
+    email: z
+      .string()
+      .min(1, 'Email required')
+      .email('Please enter a valid email'),
+    password: z.string().min(1, 'Password required'),
+    confirmPassword: z.string().min(1, 'Please confirm password'),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Passwords do not match',
+  });
+
+export type registerFormSchemaType = z.infer<typeof registerFormSchema>;
+
+const signinAndRegister = () => {
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state: RootState) => state.auth
+  );
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isError) {
+      window.alert(message);
+    }
+
+    if (user || isSuccess) {
+      navigate('/test');
+    }
+
+    dispatch(reset);
+  }, [isError, isSuccess, message, user, dispatch, navigate]);
+
   const [signIn, toggle] = useState<boolean>(true);
+  const [hidePassword, setHidePassword] = useState(true);
+
+  const {
+    register,
+    handleSubmit: registerSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<registerFormSchemaType>({
+    resolver: zodResolver(registerFormSchema),
+  });
+
+  const handleRegister: SubmitHandler<registerFormSchemaType> = async (
+    formData
+  ) => {
+    dispatch(registerUser(formData));
+  };
+
+  const {
+    register,
+    handleSubmit: loginSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<loginFormSchemaType>({
+    resolver: zodResolver(loginFormSchema),
+  });
+
+  const handleLogin: SubmitHandler<loginFormSchemaType> = async (
+    formData: loginFormSchemaType
+  ) => {
+    console.log('CLICKED LOGIN');
+    const data = dispatch(login(formData));
+
+    //useEffect not working correctly with modal
+    if ((await data).payload === 'invalid login credentials') {
+      const errMessage = (await data).payload;
+
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top',
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer);
+          toast.addEventListener('mouseleave', Swal.resumeTimer);
+        },
+      });
+      //@ts-ignore
+      Toast.fire({
+        icon: 'error',
+        title: errMessage,
+      });
+    } else {
+      navigate('/test');
+    }
+
+    dispatch(reset());
+  };
+
   return (
     <div id='signinBackground'>
       <div id='sliderContainer'>
@@ -58,58 +165,8 @@ function signinAndRegister() {
           </Components.OverlayContainer>
         </Components.Container>
       </div>
-=======
-    <div id='SliderContainer'>
-      <Components.Container>
-        <Components.SignUpContainer signinIn={signIn}>
-          <Components.Form>
-            <Components.Title>Create Account</Components.Title>
-            <Components.Input type='text' placeholder='Full Name' />
-            <Components.Input type='email' placeholder='Email' />
-            <Components.Input type='password' placeholder='Password' />
-            <Components.Input type='password' placeholder='Confirm Password' />
-            <Components.Button>Sign Up</Components.Button>
-          </Components.Form>
-        </Components.SignUpContainer>
-
-        <Components.SignInContainer signinIn={signIn}>
-          <Components.Form>
-            <Components.Title>Sign in</Components.Title>
-            <Components.Input type='email' placeholder='Email' />
-            <Components.Input type='password' placeholder='Password' />
-            <Components.Anchor href='#'>
-              Forgot your password?
-            </Components.Anchor>
-            <Components.Button>Sign In</Components.Button>
-          </Components.Form>
-        </Components.SignInContainer>
-
-        <Components.OverlayContainer signinIn={signIn}>
-          <Components.Overlay signinIn={signIn}>
-            <Components.LeftOverlayPanel signinIn={signIn}>
-              <Components.Title>Welcome Back!</Components.Title>
-              <Components.Paragraph>
-                Have an account already? Sign in to continue!
-              </Components.Paragraph>
-              <Components.GhostButton onClick={() => toggle(true)}>
-                Sign In
-              </Components.GhostButton>
-            </Components.LeftOverlayPanel>
-
-            <Components.RightOverlayPanel signinIn={signIn}>
-              <Components.Title>Hello, Friend!</Components.Title>
-              <Components.Paragraph>
-                Create an account and start generating tests!
-              </Components.Paragraph>
-              <Components.GhostButton onClick={() => toggle(false)}>
-                Sign Up
-              </Components.GhostButton>
-            </Components.RightOverlayPanel>
-          </Components.Overlay>
-        </Components.OverlayContainer>
-      </Components.Container>
     </div>
   );
-}
+};
 
 export default signinAndRegister;
