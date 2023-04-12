774c95afd8c32226a86fa0fe271cea4c576fef51
Pierce <109320021+PierceYu@users.noreply.github.com>
2023-04-11 22:34:56 -0400

    fixed test page responsiveness (#43)
    * after MVP presentation (#35)
    
    Co-authored-by: Jake Gray <gray@Jakes-MacBook-Air.local>
    
    * register UI and functionality updates (#36)
    
    * after MVP presentation
    
    * update register functionality and UI
    
    ---------
    
    Co-authored-by: Jake Gray <gray@Jakes-MacBook-Air.local>
    
    * redesign sigin and register page
    
    * added animation to signin page
    
    * added animation to signin page
    
    * fixed test page responsiveness
    
    ---------
    
    Co-authored-by: soxg <94427592+soxg@users.noreply.github.com>
    Co-authored-by: Jake Gray <gray@Jakes-MacBook-Air.local>
    Co-authored-by: jaysenjonsin <100107689+jaysenjonsin@users.noreply.github.com>

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TestHeader from '../components/TestHeader';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
@@ -13,8 +13,34 @@ type Props = {};
 const Test = (props: Props) => {
   const [userInput, setUserInput] = useState<string>('');
   const [outputTest, setOutputTest] = useState<string>('');
+  const [editorWidth, setEditorWidth] = useState('100%');
+  // const [flexDirection, setFlexDirection] = useState('column');
+  const editorRef = useRef(null);
 
-  const generateTest = async (input: string) => {
+  ///////////
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 900) {
+        setEditorWidth('45%');
+        // setFlexDirection('row');
+      } else if (window.innerWidth > 600) {
+        setEditorWidth('70%');
+        // setFlexDirection('row');
+      } else {
+        setEditorWidth('100%');
+        // setFlexDirection('column');
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  ////////////
+  const generateTest = async (input: any) => {
     try {
       console.log('clicked generateTest');
       const test = await generateTypeTest(input);
@@ -49,19 +75,89 @@ const Test = (props: Props) => {
 
   const saveTest = () => {};
 
+  const handleLeftDropDown = () => {};
+  const handleRightDropDown = () => {};
+
+  const handleEditorDidMount = (editor: any) => {
+    editorRef.current = editor;
+  };
+  const getEditorValue = () => {
+    //@ts-ignore
+    generateTest(editorRef.current.getValue());
+  };
+
   return (
     <>
       <TestHeader />
-      <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
-        <Editor height='500px' width='500px' theme='vs-dark' />
-        <Editor height='500px' width='500px' theme='vs-dark' />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          flexWrap: 'wrap',
+          /*flexDirection*/
+          flexDirection:
+            window.innerWidth > 900 || window.innerWidth > 600
+              ? 'row'
+              : 'column',
+        }}
+      >
+        {/* <div className='dropdown-menu'>
+          <select className='left-dropdown' onChange={handleLeftDropDown}>
+            <option className='dropdown-option' value='schema'>
+              Schema
+            </option>
+            <option className='dropdown-option' value='resolvers'>
+              Resolvers
+            </option>
+          </select>
+        </div> */}
+        <div className='editor-container' style={{ width: editorWidth }}>
+          <Editor
+            height='500px'
+            width='100%'
+            flex-basis='100%'
+            onMount={handleEditorDidMount}
+            theme='vs-dark'
+            language='javascript'
+            options={{
+              wordWrap: 'on',
+            }}
+          />
+        </div>
+        {/* <div className='dropdown-menu'>
+          <select className='left-dropdown' onChange={handleRightDropDown}>
+            <option className='dropdown-option' value='type-tests'>
+              Type-Tests
+            </option>
+            <option className='dropdown-option' value='unit-tests'>
+              Resolver Unit tests
+            </option>
+            <option className='dropdown-option' value='integration-tests'>
+              Integration Tests
+            </option>
+          </select>
+        </div> */}
+        <div className='editor-container' style={{ width: editorWidth }}>
+          <Editor
+            height='500px'
+            width='100%'
+            flex-basis='100%'
+            theme='vs-dark'
+            language='javascript'
+            options={{
+              wordWrap: 'on',
+            }}
+          />
+        </div>
       </Box>
 
       <Box sx={{ display: 'flex', justifyContent: 'space-around', mt: '1rem' }}>
         <Button variant='outlined' onClick={() => generateTest(userInput)}>
           Generate
         </Button>
-        <Button variant='outlined'>Save</Button>
+        <Button variant='outlined' onClick={() => getEditorValue()}>
+          Save
+        </Button>
       </Box>
     </>
   );
