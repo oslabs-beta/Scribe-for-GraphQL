271704f0ef7f5e6f8a9b652ef50c884aea6904f5
soxg <jdgray0224@gmail.com>
2023-04-17 21:21:09 -0400

    working on resolver tests

--- backend/src/controllers/resolverTestController.ts
+++ backend/src/controllers/resolverTestController.ts
@@ -10,16 +10,9 @@ export const generateResolverTests = async (
     console.log(req.body);
     // resolvers = JSON.stringify(schema)
     console.log('stringify', resolvers);
-    resolvers = schema.concat('\n resolvers;'); //need frontend to send resolvers instead of schema
+    resolvers = resolvers.concat('\n resolvers;'); //need frontend to send resolvers instead of schema
     resolvers = eval(resolvers);
     console.log('eval', resolvers.Query.allBooks);
-
-    const reqResolverJSON = {
-      resolvers:
-        'Query: { allBooks: () => books, book: (parent, { id }) => books.find(book => book.id === id), allAuthors: () => authors, author: (parent, { id }) => authors.find(author => author.id === id), allUsers: () => users, user: (parent, { id }) => users.find(user => user.id === id),}, Mutation: { createBook: (parent, { title, authorId, genre }) => { const book = { id: String(books.length + 1), title, authorId, genre }; books.push(book); return book; }, updateBook: (parent, { id, title, authorId, genre }) => { const bookIndex = books.findIndex(book => book.id === id); if (bookIndex === -1) throw new Error(`No book with id ${id}`); const book = { ...books[bookIndex], title, authorId, genre }; books[bookIndex] = book; return book;}, deleteBook: (parent, { id }) => { const bookIndex = books.findIndex(book => book.id === id); if (bookIndex === -1) throw new Error(`No book with id ${id}`); const deletedBook = books.splice(bookIndex, 1)[0]; return deletedBook;}, createUser: (parent, { name, email, age, favoriteBookId }) => { const user = { id: String(users.length + 1), name, email, age, favoriteBookId }; users.push(user); return user;}, updateUser: (parent, { id, name, email, age, favoriteBookId }) => { const userIndex = users.findIndex(user => user.id === id); if (userIndex === -1) throw new Error(`No user with id ${id}`); const user = { ...users[userIndex], name, email, age, favoriteBookId }; users[userIndex] = user; return user;}, deleteUser: (parent, { id }) => { const userIndex = users.findIndex(user => user.id === id); if (userIndex === -1) throw new Error(`No user with id ${id}`); const deletedUser = users.splice(userIndex, 1)[0]; return deletedUser;},}, Book: { author: (parent) => authors.find(author => author.id === parent.authorId),},Author: { books: (parent) => books.filter(book => book.authorId === parent.id),},User: { favoriteBook: (parent) => {if (!parent.favoriteBookId) return null; return books.find(book => book.id === parent.favoriteBookId);},},',
-    };
-
-    const reqResolver = { resolvers: JSON.parse(reqResolverJSON.resolvers) };
 
     let onlyQueries: Object[] = []; //-> {Query: { arrow function: return , arrow function return , arrow function return, arrow function return}}
 
@@ -28,7 +21,7 @@ export const generateResolverTests = async (
     let onlyResolvers: Object[] = []; //-> {}
 
     const sorter = () => {
-      for (let key of reqResolver.resolvers) {
+      for (let key of resolvers) {
         if (key === 'Query') {
           for (let funcName of key) {
             onlyQueries.push({ funcName: key[funcName] });
@@ -94,39 +87,11 @@ export const generateResolverTests = async (
       ResolverTestGenerator(onlyResolvers);
       const AllFrontBoiler = `//> npm install graphql-tools
       //> npm install casual
-      const { makeExecutableSchema, addMocksToSchema } = require('graphql-tools');
       const typeDefs = require(/* schema file */)
       const resolvers = require(/* schema file */)
-      import * as casual from "casual"
       
-      describe('Queries, mutations, and other resolvers return the correct values', () => {
-        const schema = makeExecutableSchema({ typeDefs, resolvers})
-        const mocks = { 
-          String: () => casual.sentence,
-          Int: () => casual.integer(1,100),
-          Float: () => 22.7,
-          Boolean: () => casual.boolean,
-          /*
-          If you'd like more specific mocks for any of your fields, add them below, using this as an example:
-          
-          User: () => ({ 
-            id: casual.uuid, 
-            name: casual.name, 
-            email: casual.email, 
-            age: casual.integer(18,100),
-          }),
-      
-          Please refer to the npm package 'casual' for random javascript generator calls.
-          */
-        }
-        const preserveResolvers = true;
-        const mockedSchema = addMocksToSchema({
-          schema,
-          mocks,
-          preserveResolvers
-        })`;
-      const AllEndBoiler = `
-    })`;
+        `;
+      const AllEndBoiler = ``;
       tests.unshift(AllFrontBoiler);
       tests.push(AllEndBoiler);
       return tests.toString();
@@ -137,20 +102,3 @@ export const generateResolverTests = async (
     return next(error);
   }
 };
-
-/*
-
-const testResolver = async (resolver: Function, args: any, context: Object, expected: Object) => {
-            const output = await resolver(null, args, context)
-            expect(output).toEqual(expected);
-        }
-
-        test('RESOLVERNAME returns the correct information', async () => {
-            const args = { 'USER ARG?'}
-            const context = { ' USER CONTEXT?'}
-            const expected = { ' USER EXPECTED? '}
-            const output = await testResolver(RESOLVERNAME, args, context, expected);
-        })
-
-
-*/
