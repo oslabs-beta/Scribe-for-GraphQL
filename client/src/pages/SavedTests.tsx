4236dd9a4c182278565e19c8e66e88ac2871505a
jaysenjonsin <100107689+jaysenjonsin@users.noreply.github.com>
2023-04-19 17:19:07 -0700

    Save page (#56)
    * navbar rendering and test page filtering
    
    * save test page working
    
    * saveTestInput type added

--- client/src/pages/SavedTests.tsx
+++ client/src/pages/SavedTests.tsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../app/store';
+import TestNavBar from '../components/TestNavBar';
+import { getTests, reset, Test } from '../features/testSlice';
 
 type Props = {};
 
 const SavedTests = (props: Props) => {
-  return <div>SavedTests SavedTests</div>;
+  const { tests } = useSelector((state: RootState) => state.tests);
+  const dispatch = useDispatch<AppDispatch>();
+  const [testType, setTestType] = useState('all-tests');
+  console.log('tests: ', tests);
+
+  useEffect(() => {
+    dispatch(getTests());
+
+    return () => {
+      dispatch(reset());
+    };
+  }, []);
+
+  //type-tests
+  let content;
+
+  if (testType === 'all-tests')
+    content = (
+      <div>
+        {tests.map((test: Test) => (
+          <div key={test.id}>{test.generated_test}</div>
+        ))}
+      </div>
+    );
+  else
+    content = (
+      <div>
+        {tests
+          .filter((test: Test) => test.test_type === testType)
+          .map((test: Test) => (
+            <div key={test.id}>{test.generated_test}</div>
+          ))}
+      </div>
+    );
+
+  return (
+    <>
+      <TestNavBar />
+      <div style={{ marginTop: '5rem' }}>
+        <select onChange={(e) => setTestType(e.target.value)}>
+          <option value='all-tests'>All Tests</option>
+          <option value='type-tests'>Type tests</option>
+          <option value='unit-tests'>Unit tests</option>
+          <option value='integration-tests'>Integration tests</option>
+        </select>
+        {content}
+      </div>
+    </>
+  );
 };
 
 export default SavedTests;
